2da098fe9878359d3720ccb02b2805adcaba476f
GOODFARMS-ENG <berteng90@gmail.com>
2022-08-01 15:45:26 +0800

    Added Label event

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const button = document.getElementById("button")
-button.addEventListener("click", () => {
+function inputChangeEvent() {
+    const input = document.querySelectorAll("input")
+    input.forEach(input => input.addEventListener("change", () => {
+    }))
+}
 
-})
+function validateForm() {
+    event.preventDefault();
+    const firstname = document.getElementById("firstname")
+    const lastname = document.getElementById("lastname")
+    const email = document.getElementById("email")
+    const number = document.getElementById("number")
+    const password = document.getElementById("password")
+    const confirmpass = document.getElementById("confirmpass")
+
+    if (firstname.textContent === '') {
+
+    }
+}
+
