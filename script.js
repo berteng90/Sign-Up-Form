aae8abe70ec9a9c0d1b28036f2b13f782737e8f4
GOODFARMS-ENG <berteng90@gmail.com>
2022-08-04 16:47:18 +0800

    Added Firstname Validation

--- script.js
+++ script.js
@@ -1,20 +1,77 @@
+let validate = false;
+
 function inputChangeEvent() {
+    const checkFirst = document.getElementById("firstname")
+    const checkLast = document.getElementById("lastname")
+    const checkEmail = document.getElementById("email")
+    const checkPhone = document.getElementById("phonenumber")
+    const checkPass = document.getElementById("password")
+
     const input = document.querySelectorAll("input")
-    input.forEach(input => input.addEventListener("change", () => {
+    input.forEach(input => input.addEventListener('focusout', () => {
+        if (input.id === 'firstname') {
+            const check = /^[^\W\d\s]+$/g.test(input.value)
+            console.log(check)
+            if (check === true) {
+                validate = true
+            } else {
+                console.log("TEST")
+                const div = document.querySelector(".firstname")
+                const label = document.createElement("label")
+                label.style.color = "#FF0000";
+                label.textContent = `Firstname should have no number`;
+                div.appendChild(label)
+            }
+
+        }
     }))
+
 }
 
+inputChangeEvent();
 function validateForm() {
     event.preventDefault();
-    const firstname = document.getElementById("firstname")
-    const lastname = document.getElementById("lastname")
-    const email = document.getElementById("email")
-    const number = document.getElementById("number")
-    const password = document.getElementById("password")
-    const confirmpass = document.getElementById("confirmpass")
-
-    if (firstname.textContent === '') {
-
-    }
+    const input = document.querySelectorAll("input")
+    input.forEach(input => {
+        if (input.value === "" && input.id === "firstname") {
+            const div = document.querySelector(".firstname")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Firstname is required!`;
+            div.appendChild(label)
+        }
+        else if (input.value === "" && input.id === "lastname") {
+            const div = document.querySelector(".lastname")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Lastname is required!`;
+            div.appendChild(label)
+        } else if (input.value === "" && input.id === "email") {
+            const div = document.querySelector(".email")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Email is required!`;
+            div.appendChild(label)
+        } else if (input.value === "" && input.id === "phonenumber") {
+            const div = document.querySelector(".phonenumber")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Phone number is required!`;
+            div.appendChild(label)
+        } else if (input.value === "" && input.id === "password") {
+            const div = document.querySelector(".password")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Password is required!`;
+            div.appendChild(label)
+        }
+        else if (input.value === "" && input.id === "confirmpass") {
+            const div = document.querySelector(".confirm")
+            const label = document.createElement("label")
+            label.style.color = "#FF0000";
+            label.textContent = `Password does not match!`;
+            div.appendChild(label)
+        }
+    })
 }
 
